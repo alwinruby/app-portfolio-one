086f8875ea10d43843802a27690dc8fc632095dc
asolanky <alwinsolanky@gmail.com>
2021-07-03 06:11:33 +0100

    Updated version 2.0

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -44,7 +44,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I have <br/>Writen ... before an old blog on JavaScript, its been a while.</h1>
+                          <h1>I have <br/>Writen before, an old blog on JavaScript.</h1>
                           <p><a className="btn btn-primary btn-learn" href="http://devcodeadventures.com/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
